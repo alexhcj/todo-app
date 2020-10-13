4a8abc4b98ae93160d6967c679efcf0d368f1dd7
alex_hcj <alexhcj@yandex.ru>
2020-10-13 05:01:11 +0300

    Finished video tutorial. Build simple crud todo on firebase.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core'
+import Todo from './Todo'
+import db from './firebase'
+import firebase from 'firebase'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todos, setTodos] = useState(['abc', 'def'])
+	const [input, setInput] = useState('')
+
+	useEffect(() => {
+		db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+			setTodos(snapshot.docs.map(doc => ({id: doc.id, todo: doc.data().todo})))
+		})
+	}, [])
+	
+	const addTodo = (event) => {
+		event.preventDefault()
+
+		db.collection('todos').add({
+			todo: input,
+			timestamp: firebase.firestore.FieldValue.serverTimestamp()
+		})
+		
+		setTodos([...todos, input])
+		setInput('')
+	}
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h1>Hellow World!</h1>
+				<form>
+					<FormControl>
+						<InputLabel>Write a Todo</InputLabel>
+						<Input
+							value={input}
+							onChange={(event) => setInput(event.target.value)}
+							type='text'
+						></Input>
+					</FormControl>
+
+					<Button
+						disabled={!input}
+						onClick={addTodo}
+						type='submit'
+						variant='contained'
+						color='primary'
+					>
+						Add Todo
+					</Button>
+				</form>
+				<ul>
+					{todos.map((todo, index) => (
+						<Todo todo={todo} key={index} />
+					))}
+				</ul>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
